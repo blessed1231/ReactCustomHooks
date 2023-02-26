fc32547123e58cb9815d0498b0bf93e554e70dd4
blessed1231 <106454877+blessed1231@users.noreply.github.com>
2023-02-26 14:10:00 +0100

    Add files via upload
    4 stupid hooks for nothing

---
+++ useScroll.js
@@ -0,0 +1,26 @@
+import {useEffect, useRef} from "react";
+
+export default function useScroll(parentRef, childrenRef, callback) {
+     const observer = useRef()
+
+    useEffect(() => {
+        const  options = {
+            root: parentRef.current,
+            rootMargin: '0px',
+            threshold: 0
+        }
+        observer.current = new IntersectionObserver(([target]) => {
+            if (target.isIntersecting) {
+                console.log('inter')
+                callback()
+            }
+        }, options)
+
+        observer.current.observe(childrenRef.current)
+
+
+        return function () {
+            observer.current.unobserve(childrenRef.current)
+        }
+    }, [callback])
+}
